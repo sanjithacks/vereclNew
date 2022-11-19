92df21ea94ebd7e7b7050e0360278602f0564162
SANJIT <35589762+sanjithacks@users.noreply.github.com>
2022-11-19 18:23:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 // This is an async function
 app.get("/", async (req, reply) => {
   //await reply.status(200).render("index");
-  await reply.render(path.join(__dirname + '/views/index'))
+  await reply.render(path.join(__dirname + '/views/index.ejs'))
 });
 
 app.use(async (req, res, next) => {
