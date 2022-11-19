6aca8ff417ba55f743bf88baebcf4a345cb0e4e7
SANJIT <35589762+sanjithacks@users.noreply.github.com>
2022-11-19 18:04:51 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.json());
