978ddeaf3498929f0daf525ff9803fc45e0e61cc
SANJIT <35589762+sanjithacks@users.noreply.github.com>
2022-11-19 18:05:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.json());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.options("*", cors());
+//app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+/*Self def*/
+
+// This is an async function
+app.get("/", async (req, reply) => {
+  await reply.status(200).render("index");
+});
+
+app.use(async (req, res, next) => {
+  try {
+    await res.status(404).end("Not Found");
+  } catch (error) {
+    await res.status(500).end("Server error");
+  }
+});
+
+app.listen(PORT, function () {
+  //console.log("server running on port hurray" + PORT, "http://localhost:" + PORT);
+});
